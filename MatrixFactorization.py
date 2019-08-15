2298781609fa415d997e0d70a7c5d376dd6efc3e
Ananya-Bhattacharjee <ananyab153258@gmail.com>
2019-08-15 22:56:01 +0600

    Update MatrixFactorization.py

--- MatrixFactorization.py
+++ MatrixFactorization.py
@@ -2,22 +2,22 @@ import easygui
 
 def mat_factorization(R, P, Q, K, steps=10000, alpha=0.002, beta=0.02):
     Q = Q.T
-    for step in xrange(steps):
-        #print step
-        for i in xrange(len(R)):
-            for j in xrange(len(R[i])):
+    for step in range(steps):
+        print (step)
+        for i in range(len(R)):
+            for j in range(len(R[i])):
                 if R[i][j] >= 0 and i>=j:
                     eij = R[i][j] - numpy.dot(P[i,:],Q[:,j])
-                    for k in xrange(K):
+                    for k in range(K):
                         P[i][k] = P[i][k] + alpha * (2 * eij * Q[k][j] - beta * P[i][k])
                         Q[k][j] = Q[k][j] + alpha * (2 * eij * P[i][k] - beta * Q[k][j])
         eR = numpy.dot(P,Q)
         e = 0
-        for i in xrange(len(R)):
-            for j in xrange(len(R[i])):
+        for i in range(len(R)):
+            for j in range(len(R[i])):
                 if R[i][j] > 0:
                     e = e + pow(R[i][j] - numpy.dot(P[i,:],Q[:,j]), 2)
-                    for k in xrange(K):
+                    for k in range(K):
                         e = e + (beta/2) * ( pow(P[i][k],2) + pow(Q[k][j],2) )
         if e < 0.000001:
             break
